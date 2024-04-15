34ad11104118b12991ee6872f440e35db5597e96
ByeongGukKang <bgkang0524@gmail.com>
2024-04-15 21:23:26 +0900

    [project 2] adjust trap.c for (v)runtime update debug1

--- trap.c
+++ trap.c
@@ -103,11 +103,6 @@ trap(struct trapframe *tf)
   // Force process to give up CPU on clock tick.
   // If interrupts were on while locks held, would need to check nlock.
   if(myproc() && myproc()->state == RUNNING && tf->trapno == T_IRQ0+IRQ_TIMER) {
-    uint newruntime = myproc()->runtime + 1000;
-    uint currvruntime = myproc()->vruntime;
-    uint deltavruntime = 1024000/wgtarr[myproc()->nice];
-    myproc()->runtime = newruntime;
-    myproc()->vruntime = currvruntime + deltavruntime;
     yield();
   }
 
